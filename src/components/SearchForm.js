70ad8188d578051fa7ff3e1715ce66260e3465bb
Robert Stepanov <robertdstepanov@gmail.com>
2019-12-20 18:54:03 -0500

    Search Bug

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from "react";
 
 export default function SearchForm(props) {
   const [searchName, setSearchName] = useState("");
-
+  const [characters, setCharacters] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
+  // const below when commented out breaks it
+  // props not being passed down correctly
+  // const characters = [];
   useEffect(() => {
-    const characters = [];
     const results = characters.filter(character =>
-      character.name.toLowerCase().includes(searchName.toLocaleLowerCase())
+      character.name.toLowerCase().includes(searchName.toLowerCase())
     );
     setSearchResults(results);
-  }, [searchName]);
+    console.log(results);
+  }, [characters]);
 
   const handleChange = event => {
     setSearchName(event.target.value);
@@ -23,14 +26,14 @@ export default function SearchForm(props) {
         <input
           id="name"
           type="text"
-          name="textfield"
+          // name="textfield"
           placeholder="Search"
           value={searchName}
           onChange={handleChange}
         />
       </form>
       <div>
-        <ul>
+        <ul key="id">
           {searchResults.map(character => (
             <li key={character}>{character}</li>
           ))}
